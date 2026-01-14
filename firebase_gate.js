351c8aec354a51a60c00b1a10de83152301592c7
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-01-14 01:10:30 -0500

    Update firebase_gate.js

--- firebase_gate.js
+++ firebase_gate.js
@@ -1,1 +1,25 @@
+(function () {
+  "use strict";
 
+  // Feature flag
+  const FIREBASE_ENABLED = false; // <-- stays false until everything works
+
+  function log(msg) {
+    console.log("[FirebaseGate]", msg);
+  }
+
+  function waitForCore() {
+    if (window.MM_CORE_READY === true) {
+      log("Core detected. Safe to proceed.");
+      if (FIREBASE_ENABLED) initFirebase();
+      return;
+    }
+    setTimeout(waitForCore, 50);
+  }
+
+  function initFirebase() {
+    log("Firebase init skipped (disabled).");
+  }
+
+  waitForCore();
+})();
