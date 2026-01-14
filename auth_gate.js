73d407d6ed5c03ea32fa6d2bf0fa1302a4d13857
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-01-14 11:23:53 -0500

    Update auth_gate.js

--- auth_gate.js
+++ auth_gate.js
@@ -1,1 +1,44 @@
+(function () {
+  "use strict";
 
+  /**
+   * Auth Gate
+   * =========
+   * This file decides IF the app is allowed to start.
+   * It does NOT touch app logic.
+   * It does NOT modify state.
+   * It only listens and reports.
+   */
+
+  function log(msg) {
+    console.log("[AuthGate]", msg);
+  }
+
+  // We assume "not signed in" by default
+  window.MM_AUTH_READY = false;
+
+  function waitForFirebase() {
+    if (!window.firebase || !firebase.auth) {
+      log("Waiting for Firebase Auth...");
+      return setTimeout(waitForFirebase, 100);
+    }
+
+    log("Firebase Auth detected");
+    listenForAuth();
+  }
+
+  function listenForAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        log("User signed in:", user.email || user.uid);
+        window.MM_AUTH_READY = true;
+      } else {
+        log("No user signed in");
+        window.MM_AUTH_READY = false;
+      }
+    });
+  }
+
+  // Start watching
+  waitForFirebase();
+})();
